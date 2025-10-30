c710f8c45c3c903b8696898fba1f0e71067e926c
galenograficos <galenograficos@gmail.com>
2025-10-30 16:07:10 +0000

    NavMenu con 3 rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { Locals } from './vistas/locals/locals';
+import { Contac } from './vistas/contac/contac';
+import { Home } from './vistas/home/home';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+
+    { path: '', component: NavMenuComponent,
+        children: [
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: 'home', component: Home },
+            { path: 'locals', component: Locals },
+            { path: 'contac', component: Contac },
+        ]
+     }
+
+];
---
+++ src/app/nav-menu/nav-menu.component.spec.ts
@@ -0,0 +1,31 @@
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NavMenuComponent } from './nav-menu.component';
+
+describe('NavMenuComponent', () => {
+  let component: NavMenuComponent;
+  let fixture: ComponentFixture<NavMenuComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [NavMenuComponent],
+      imports: [
+        MatButtonModule,
+        MatIconModule,
+        MatListModule,
+        MatSidenavModule,
+        MatToolbarModule,
+      ]
+    });
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NavMenuComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should compile', () => {
+    expect(component).toBeTruthy();
+  });
+});
