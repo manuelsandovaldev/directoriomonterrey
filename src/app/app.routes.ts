81b95d1b5cf959a714fc319089eabb891c76ee78
galenograficos <galenograficos@gmail.com>
2025-11-10 20:50:34 +0000

    agrege una nueva vista llamada  NOVEDADES

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,25 +5,21 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { Locals } from './vistas/locals/locals';
 import { Contac } from './vistas/contac/contac';
 import { Home } from './vistas/home/home';
+import { Novedades } from './components/novedades/novedades';
 
 
 export const routes: Routes = [
 
     { 
-        // 1. Ruta Padre: Carga el NavMenuComponent en el <router-outlet /> principal
-        path: '', 
-        component: NavMenuComponent,
+        path: '', component: NavMenuComponent,
         
         children: [
-            // 2. Ruta Hija por defecto: Carga el HomeComponent DIRECTAMENTE en la ruta vacía
-            { path: '', component: Home }, 
             
-            // 3. Rutas de contenido que se cargarán dentro del NavMenuComponent
+            { path: '', component: Home }, 
             { path: 'home', component: Home },
             { path: 'locals', component: Locals },
             { path: 'contac', component: Contac },
-            
-            // 4. Manejo del comodín dentro del layout
+            { path: 'novedades', component: Novedades },
             { path: '**', redirectTo: 'home' } 
         ]
     }
